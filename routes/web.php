9239e1d10cbfbe0729eb4411f581bac435cecacb
fewza55 <78992795+fewza55@users.noreply.github.com>
2021-02-13 13:30:06 +0700

    ใส่ homecontroll

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,17 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class,'index']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [HomeController::class,'about']);
 
-Route::get('/contact', function () {
-    return view('contract');
-});
+Route::get('/contact', [HomeController::class,'contact']);
 
 Route::get('/หวัดดีเบล', function () {
     return "แกรีบกลับมาได้แล้วนะโว้ย";
@@ -31,4 +26,8 @@ Route::get('/หวัดดีเบล', function () {
 });
 Route::get('/hello/{name}', function ($name) {
     return "hello hello เฮ้ดอีหยังอยู่น้อ อี $name";
+});
+
+Route::get('/calculator/{num1}/{num2}', function ($num1,$num2) {
+    return "ผลบวกของเลข $num1 และ $num2 คือ " . ($num1 + $num2);
 });
